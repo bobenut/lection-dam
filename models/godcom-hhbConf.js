3c136a1de787692cc2269ce797ab758a04f705c9
bobenut <33755383@qq.com>
2017-08-07 16:15:47 +0800

    completed godcom base data

---
+++ models/godcom-hhbConf.js
@@ -0,0 +1,282 @@
+var hhbConf = {
+    domain: 'www.godcom.net',
+    port: 80,
+    url: '/hhb',
+    oldTestamentPieces: [
+        {
+            no: 1,
+            name_cn: '创世纪',
+            name_en: 'Genesis',
+            url: 'B01C001.htm',
+            sectionNum: 50
+        },
+        {
+            no: 2,
+            name_cn: '出埃及记',
+            name_en: 'Exodus',
+            url: 'B02C001.htm',
+            sectionNum: 40
+        },
+        {
+            no: 3,
+            name_cn: '利未记',
+            name_en: 'Leviticus',
+            url: 'B03C001.htm',
+            sectionNum: 27
+        },
+        {
+            no: 4,
+            name_cn: '民数记 ',
+            name_en: 'Numbers',
+            url: 'B04C001.htm',
+            sectionNum: 36
+        },
+        {
+            no: 5,
+            name_cn: '申命记',
+            name_en: 'Deuteronomy',
+            url: 'B05C001.htm',
+            sectionNum: 34
+        },
+        {
+            no: 6,
+            name_cn: '约书亚记',
+            name_en: 'Joshua',
+            url: 'B06C001.htm',
+            sectionNum: 24
+        },
+        {
+            no: 7,
+            name_cn: '士师记',
+            name_en: 'Judges',
+            url: 'B07C001.htm',
+            sectionNum: 21
+        },
+        {
+            no: 8,
+            name_cn: '路得记 ',
+            name_en: 'Ruth',
+            url: 'B08C001.htm',
+            sectionNum: 4
+        },
+        {
+            no: 9,
+            name_cn: '撒母耳记上',
+            name_en: 'Samuel 1',
+            url: 'B09C001.htm',
+            sectionNum: 31
+        },
+        {
+            no: 10,
+            name_cn: '撒母耳记下',
+            name_en: 'Samuel 2',
+            url: 'B10C001.htm',
+            sectionNum: 24
+        },
+        {
+            no: 11,
+            name_cn: '列王记上',
+            name_en: 'Kings 1',
+            url: 'B11C001.htm',
+            sectionNum: 22
+        },
+        {
+            no: 12,
+            name_cn: '列王记下',
+            name_en: 'Kings 2',
+            url: 'B12C001.htm',
+            sectionNum: 25
+        },
+        {
+            no: 13,
+            name_cn: '历代志上',
+            name_en: 'Chronicles 1',
+            url: 'B13C001.htm',
+            sectionNum: 29
+        },
+        {
+            no: 14,
+            name_cn: '历代志下',
+            name_en: 'Chronicles 2',
+            url: 'B14C001.htm',
+            sectionNum: 36
+        },
+        {
+            no: 15,
+            name_cn: '以斯拉记',
+            name_en: 'Ezra',
+            url: 'B15C001.htm',
+            sectionNum: 10
+        },
+        {
+            no: 16,
+            name_cn: '尼希米记',
+            name_en: 'Nehemiah',
+            url: 'B16C001.htm',
+            sectionNum: 13
+        },
+        {
+            no: 17,
+            name_cn: '以斯帖记',
+            name_en: 'Esther',
+            url: 'B17C001.htm',
+            sectionNum: 10
+        },
+        {
+            no: 18,
+            name_cn: '约伯记',
+            name_en: 'Job',
+            url: 'B18C001.htm',
+            sectionNum: 42
+        },
+        {
+            no: 19,
+            name_cn: '诗篇',
+            name_en: 'Psalms',
+            url: 'B19C001.htm',
+            sectionNum: 150
+        },
+        {
+            no: 20,
+            name_cn: '箴言',
+            name_en: 'Proverbs',
+            url: 'B20C001.htm',
+            sectionNum: 31
+        },
+        {
+            no: 21,
+            name_cn: '传道书',
+            name_en: 'Ecclesiastes',
+            url: 'B21C001.htm',
+            sectionNum: 12
+        },
+        {
+            no: 22,
+            name_cn: '雅歌',
+            name_en: 'Song of Songs',
+            url: 'B22C001.htm',
+            sectionNum: 8
+        },
+        {
+            no: 23,
+            name_cn: '以赛亚书',
+            name_en: 'Isaiah',
+            url: 'B23C001.htm',
+            sectionNum: 66
+        },
+        {
+            no: 24,
+            name_cn: '耶利米书',
+            name_en: 'Jeremiah',
+            url: 'B24C001.htm',
+            sectionNum: 52
+        },
+        {
+            no: 25,
+            name_cn: '耶利米哀歌',
+            name_en: 'Lamentations',
+            url: 'B25C001.htm',
+            sectionNum: 5
+        },
+        {
+            no: 26,
+            name_cn: '以西结书',
+            name_en: 'Ezekiel',
+            url: 'B26C001.htm',
+            sectionNum: 48
+        },
+        {
+            no: 27,
+            name_cn: '但以理书',
+            name_en: 'Daniel',
+            url: 'B27C001.htm',
+            sectionNum: 12
+        },
+        {
+            no: 28,
+            name_cn: '何西阿书',
+            name_en: 'Hosea',
+            url: 'B28C001.htm',
+            sectionNum: 14
+        },
+        {
+            no: 29,
+            name_cn: '约珥书',
+            name_en: 'Joel',
+            url: 'B29C001.htm',
+            sectionNum: 3
+        },
+        {
+            no: 30,
+            name_cn: '阿摩司书',
+            name_en: 'Amos',
+            url: 'B30C001.htm',
+            sectionNum: 9
+        },
+        {
+            no: 31,
+            name_cn: '俄巴底亚书',
+            name_en: 'Obadiah',
+            url: 'B31C001.htm',
+            sectionNum: 1
+        },
+        {
+            no: 32,
+            name_cn: '约拿书',
+            name_en: 'Jonah',
+            url: 'B32C001.htm',
+            sectionNum: 4
+        },
+        {
+            no: 33,
+            name_cn: '弥迦书',
+            name_en: 'Micah',
+            url: 'B33C001.htm',
+            sectionNum: 7
+        },
+        {
+            no: 34,
+            name_cn: '那鸿书',
+            name_en: 'Nahum',
+            url: 'B34C001.htm',
+            sectionNum: 3
+        },
+        {
+            no: 35,
+            name_cn: '哈巴谷书',
+            name_en: 'Habakkuk',
+            url: 'B35C001.htm',
+            sectionNum: 3
+        },
+        {
+            no: 36,
+            name_cn: '西番雅书',
+            name_en: 'Zephaniah',
+            url: 'B36C001.htm',
+            sectionNum: 3
+        },
+        {
+            no: 37,
+            name_cn: '哈该书',
+            name_en: 'Haggai',
+            url: 'B37C001.htm',
+            sectionNum: 2
+        },
+        {
+            no: 38,
+            name_cn: '撒迦利亚',
+            name_en: 'Zechariah',
+            url: 'B38C001.htm',
+            sectionNum: 14
+        },
+        {
+            no: 39,
+            name_cn: '玛拉基书',
+            name_en: 'Malachi',
+            url: 'B39C001.htm',
+            sectionNum: 4
+        }
+    ]
+}
+
+module.exports = hhbConf;
